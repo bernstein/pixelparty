4a60fd4ee553378cfd0191f71e009aefd6f4d2ae
Andreas-Christoph Bernstein <andreas.bernstein@googlemail.com>
2011-06-04 18:38:30 +0200

    small changes

--- src/PixelParty/Texture2D.hs
+++ src/PixelParty/Texture2D.hs
@@ -4,16 +4,15 @@ where
 
 import Control.Applicative ((<$>), pure)
 import Data.Array.Unboxed
-import qualified Graphics.UI.GLUT as GLUT
 import qualified Graphics.Rendering.OpenGL.Raw as GL
 import Foreign.Storable (Storable, sizeOf)
-import Foreign.Marshal.Array (allocaArray, peekArray, withArrayLen)
-import Foreign (nullPtr, withArray, plusPtr, sizeOf, castPtr, Ptr, withMany)
+import Foreign.Marshal.Array (withArray, allocaArray, peekArray, withArrayLen)
+import Foreign (nullPtr, plusPtr, sizeOf, castPtr, Ptr, withMany)
 import Data.Array.MArray (thaw)
 import Data.Array.Storable (withStorableArray)
 
 import Data.Bitmap.IO
-import Codec.Image.STB
+import Codec.Image.STB (loadImage)
 
 type GLTextureUnit = GL.GLenum
 type GLTextureObject = GL.GLuint
@@ -27,14 +26,18 @@ loadTextureOld path u = do
       GL.glActiveTexture u
       t <- fmap head $ allocaArray 1 (\buf -> GL.glGenTextures 1 buf >> peekArray 1 buf)
       GL.glBindTexture GL.gl_TEXTURE_2D t
-      GL.glTexParameteri GL.gl_TEXTURE_2D GL.gl_TEXTURE_MIN_FILTER (fromIntegral GL.gl_LINEAR)
-      GL.glTexParameteri GL.gl_TEXTURE_2D GL.gl_TEXTURE_MAG_FILTER (fromIntegral GL.gl_LINEAR)
+      GL.glTexParameteri GL.gl_TEXTURE_2D GL.gl_TEXTURE_MIN_FILTER 
+                                        (fromIntegral GL.gl_LINEAR)
+      GL.glTexParameteri GL.gl_TEXTURE_2D GL.gl_TEXTURE_MAG_FILTER 
+                                        (fromIntegral GL.gl_LINEAR)
 
       withBitmap bm $ \(width,height) nchn pad ptr -> do
-        let ty = dataType2 bm
+        let ty = marshalPixelComponent bm
             (pf,pif) = formatPlusInternalFormat bm
-        GL.glPixelStorei GL.gl_UNPACK_ALIGNMENT (fromIntegral (bitmapRowAlignment bm))
-        GL.glTexImage2D GL.gl_TEXTURE_2D 0 pif (fromIntegral width) (fromIntegral height) 0 pf ty ptr
+            (w,h) = (fromIntegral width, fromIntegral height)
+            alignment = fromIntegral (bitmapRowAlignment bm)
+        GL.glPixelStorei GL.gl_UNPACK_ALIGNMENT alignment
+        GL.glTexImage2D GL.gl_TEXTURE_2D 0 pif w h 0 pf ty ptr
 
       return (t,u)
 
@@ -45,12 +48,9 @@ enableTexture (t,u) = do
   GL.glBindTexture GL.gl_TEXTURE_2D t
 
 -- -----------------------------------------------------------------------------
--- OpenGL data type
-dataType2 :: forall t. PixelComponent t => Bitmap t -> GL.GLenum
-dataType2 _ = marshalPixelComponent (undefined::t)
 
-marshalPixelComponent :: PixelComponent t => t -> GL.GLenum
-marshalPixelComponent t = case pixelComponentType t of
+marshalPixelComponent :: forall t.PixelComponent t => Bitmap t -> GL.GLenum
+marshalPixelComponent _ = case pixelComponentType (undefined::t) of
   PctWord8  -> GL.gl_UNSIGNED_BYTE
   PctWord16 -> GL.gl_UNSIGNED_SHORT
   PctWord32 -> GL.gl_UNSIGNED_INT
@@ -60,15 +60,13 @@ formatPlusInternalFormat :: forall t. PixelComponent t => Bitmap t -> (GL.GLenum
 formatPlusInternalFormat bm = 
   case pixelComponentType (undefined::t) of 
         PctWord8 -> case bitmapNChannels bm of
-        --  1 -> (GL.gl_ALPHA, GL.gl_ALPHA8)
-        --  2 -> (GL.gl_LUMINANCE_ALPHA, fromIntegral GL.gl_LUMINANCE8_ALPHA8)
+          1 -> (GL.gl_ALPHA, fromIntegral GL.gl_ALPHA8)
+          2 -> (GL.gl_LUMINANCE_ALPHA, fromIntegral GL.gl_LUMINANCE8_ALPHA8)
           3 -> (GL.gl_RGB, fromIntegral GL.gl_RGB8)
           4 -> (GL.gl_RGBA, fromIntegral GL.gl_RGBA8)  
         _ -> case bitmapNChannels bm of
-        --  1 -> (GL.gl_ALPHA, fromIntegral GL.gl_ALPHA)
-        --  2 -> (GL.gl_LUMINANCE_ALPHA, fromIntegral GL.gl_LUMINANCE_ALPHA)
+          1 -> (GL.gl_ALPHA, fromIntegral GL.gl_ALPHA)
+          2 -> (GL.gl_LUMINANCE_ALPHA, fromIntegral GL.gl_LUMINANCE_ALPHA)
           3 -> (GL.gl_RGB, fromIntegral GL.gl_RGB)
           4 -> (GL.gl_RGBA, fromIntegral GL.gl_RGBA)  
-
-
 
